e9e852e851518d11db4094199ac720bf0d947ded
Emirhan Latif <emirhanlatif@outlook.com>
2021-02-01 18:59:50 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2018,DailyPrice=100000,Description="Kırığı Çiziği vs. yoktur."},
+                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=2015,DailyPrice=50000,Description="Kaportada çizik vardır"},
+                new Car{Id=3,BrandId=2,ColorId=3,ModelYear=2012,DailyPrice=600000,Description="Renk değişimi yapılmıştır"},
+                new Car{Id=4,BrandId=2,ColorId=4,ModelYear=2011,DailyPrice=700000,Description="Kazası vardır"},
+                new Car{Id=5,BrandId=3,ColorId=5,ModelYear=2013,DailyPrice=800000,Description="10000 kilometrededir."}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
